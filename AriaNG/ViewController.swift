baac1f08ccfabe79ea5e0dbd818377083e43564f
Vinken <vinkencheng@gmail.com>
2018-03-01 12:05:51 +0800

    Initial Commit

---
+++ AriaNG/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AriaNG
+//
+//  Created by Vinken Chan on 1/3/2018.
+//  Copyright © 2018年 Vinken Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
